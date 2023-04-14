96be61e5c2cbc87d8ab0b68475bca8909a1dfd1b
Justin Wagoner <justin.wagoner@exchange.koriist.com>
2023-04-14 11:39:25 -0400

    basic capability to add a game session

--- routes/game_session.js
+++ routes/game_session.js
@@ -1,14 +1,21 @@
 module.exports = {
 	getAdd: (req, res) => {
-		res.render('add-game-session.ejs', {
-			title: 'Board Games | Add game session'
+		db.query('SELECT * FROM Games ORDER BY game_name ASC', (err, results) => {
+			if (err) throw err;
+			res.render('add-game-session.ejs', {
+				title: 'Board Games | Add game session',
+				games: results
+			});
 		});
 	},
 	postAdd: (req, res) => {
-		console.log(req.body);
+		let { game, startDate } = req.body;
+		let query = `INSERT INTO GameSessions (game_id, game_session_start_date) VALUES ('${game}', '${startDate}')`;
 
-		// TODO db.query to insert game-playing session
-
-		res.redirect('/');
+		db.query(query, (err, result) => {
+			if (err) throw err;
+			console.log(`Game session added for ${game}`);
+			res.redirect('/');
+		});
 	}
 };
