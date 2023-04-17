7938260a0fa9250f99b7015a9f0746165c83e2bb
Justin Wagoner <justin.wagoner@exchange.koriist.com>
2023-04-17 15:46:46 -0400

    cleaned up code by allowing to read in the schema.sql file instead of needing to have each query in app.js

--- app.js
+++ app.js
@@ -40,109 +40,12 @@ db.query('SHOW TABLES LIKE "Games";', function (error, results, fields) {
     if (results.length == 0) {
         console.log('Database does not contain the table "Games" and "GameSessions", creating it now and filling with data');
         const schema = fs.readFileSync('./schema.sql', 'utf8');
-        db.query("CREATE TABLE Games (game_id INT NOT NULL AUTO_INCREMENT,game_name VARCHAR(255) NOT NULL,game_image LONGBLOB,PRIMARY KEY (game_id));", function (error, results, fields) {
-            if (error){
-                throw error;
-            } 
-            console.log('Table Games created');
+        db.query(schema, function (error, results, fields) {
+          if (error){
+            throw error;
+          } 
+          console.log('Tables created and data added');
         });
-        db.query("CREATE TABLE GameSessions (game_session_id INT NOT NULL AUTO_INCREMENT,game_id INT NOT NULL,game_session_start_date DATETIME NOT NULL,PRIMARY KEY (game_session_id),FOREIGN KEY (game_id) REFERENCES Games(game_id) ON DELETE CASCADE);", function (error, results, fields) {
-            if (error){
-                throw error;
-            } 
-            console.log('Table GameSessions created');
-        });
-        db.query("INSERT INTO Games (game_name, game_image) VALUES ('Monopoly', 'https://toppng.com/uploads/preview/monopoly-collect-win-transparent-background-monopoly-guy-11563049600zctdjngr0t.png');", function (error, results, fields) {
-            if (error){
-                throw error;
-            } 
-            console.log('Table Game recorded added');
-        });
-        db.query("INSERT INTO Games (game_name, game_image) VALUES ('Othello', 'https://png.pngtree.com/png-vector/20210912/ourlarge/pngtree-go-othello-gomoku-png-image_3913163.jpg');", function (error, results, fields) {
-            if (error){
-                throw error;
-            } 
-            console.log('Table Game recorded added');
-        });
-        db.query("INSERT INTO Games (game_name, game_image) VALUES ('Catan', 'https://p7.hiclipart.com/preview/692/115/598/catan-boardgamegeek-dice-board-game-dice-thumbnail.jpg');", function (error, results, fields) {
-            if (error){
-                throw error;
-            } 
-            console.log('Table Game recorded added');
-        });
-        db.query("INSERT INTO Games (game_name, game_image) VALUES ('Cards Against Humanity', 'https://www.vhv.rs/dpng/d/465-4655968_cards-against-humanity-png-cards-against-humanity-packaging.png');", function (error, results, fields) {
-            if (error){
-                throw error;
-            } 
-            console.log('Table Game recorded added');
-        });
-        db.query("INSERT INTO Games (game_name, game_image) VALUES ('Ticket to Ride', 'https://banner2.cleanpng.com/20180629/hsk/kisspng-days-of-wonder-ticket-to-ride-series-board-game-pa-product-box-5b36f24fd48354.8126638415303276318705.jpg');", function (error, results, fields) {
-            if (error){
-                throw error;
-            } 
-            console.log('Table Game recorded added');
-        });
-        db.query("INSERT INTO GameSessions (game_id, game_session_start_date) VALUES (1, '2023-04-01 08:00:00');", function (error, results, fields) {
-            if (error){
-                throw error;
-            } 
-            console.log('Table Game Session recorded added');
-        });
-        db.query("INSERT INTO GameSessions (game_id, game_session_start_date) VALUES (2, '2023-04-01 09:00:00');", function (error, results, fields) {
-            if (error){
-                throw error;
-            } 
-            console.log('Table Game Session recorded added');
-        });
-        db.query("INSERT INTO GameSessions (game_id, game_session_start_date) VALUES (2, '2023-04-01 10:00:00');", function (error, results, fields) {
-            if (error){
-                throw error;
-            } 
-            console.log('Table Game Session recorded added');
-        });
-        db.query("INSERT INTO GameSessions (game_id, game_session_start_date) VALUES (4, '2023-04-01 11:00:00');", function (error, results, fields) {
-            if (error){
-                throw error;
-            } 
-            console.log('Table Game Session recorded added');
-        });
-        db.query("INSERT INTO GameSessions (game_id, game_session_start_date) VALUES (5, '2023-04-01 12:00:00');", function (error, results, fields) {
-            if (error){
-                throw error;
-            } 
-            console.log('Table Game Session recorded added');
-        });
-        db.query("INSERT INTO GameSessions (game_id, game_session_start_date) VALUES (1, '2023-04-08 08:00:00');", function (error, results, fields) {
-            if (error){
-                throw error;
-            } 
-            console.log('Table Game Session recorded added');
-        });
-        db.query("INSERT INTO GameSessions (game_id, game_session_start_date) VALUES (2, '2023-04-08 09:00:00');", function (error, results, fields) {
-            if (error){
-                throw error;
-            } 
-            console.log('Table Game Session recorded added');
-        });
-        db.query("INSERT INTO GameSessions (game_id, game_session_start_date) VALUES (3, '2023-04-08 10:00:00');", function (error, results, fields) {
-            if (error){
-                throw error;
-            } 
-            console.log('Table Game Session recorded added');
-        });
-        db.query("INSERT INTO GameSessions (game_id, game_session_start_date) VALUES (4, '2023-04-08 11:00:00');", function (error, results, fields) {
-            if (error){
-                throw error;
-            } 
-            console.log('Table Game Session recorded added');
-        });
-        db.query("INSERT INTO GameSessions (game_id, game_session_start_date) VALUES (5, '2023-04-08 12:00:00');", function (error, results, fields) {
-            if (error){
-                throw error;
-            } 
-            console.log('Table Game Session recorded added');
-        });
-
     } 
     // if they do exist prompt user to kill the db and restart
     else {
@@ -153,7 +56,8 @@ db.query('SHOW TABLES LIKE "Games";', function (error, results, fields) {
 app.set('port', process.env.port || port);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // If there are static files, make a public directory
 app.use(express.static(path.join(__dirname, 'public')));
--- config.js
+++ config.js
@@ -1,10 +1,12 @@
 module.exports = {
   port: 3000,
   db: {
+    //host: 'db',
     host: 'miedb',
-    //port: 3307,
+    //port: 3306,
     user: 'app',
     password: 'wonderful',
-    database: 'miechallenge'
+    database: 'miechallenge',
+    multipleStatements: true
   }
 }
